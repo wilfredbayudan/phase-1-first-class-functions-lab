b31bbb974c7d79f759fef17497d69ec33dae0511
Mohammed Shaikh <46161085+mqshaikh8@users.noreply.github.com>
2021-07-12 18:57:29 -0400

    commit tree 745a3da808a4f3e189af1c5bd77012780bb3a143

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Code your solution in this file!
+const drivers = ['Sally', 'Bob', 'Freddy', 'Claudia'];
+
+const returnFirstTwoDrivers = function(drivers){
+    return drivers.slice(0,2)
+}// Code your solution in this file!
+
+const returnLastTwoDrivers = function(drivers){
+    return drivers.slice( -2)
+}
+
+// const selectingDrivers = function(){
+//     return returnFirstTwoDrivers().slice(0,1)
+// }
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+const createFareMultiplier = function(fare){
+    return function (value){
+        return fare * value
+    }
+
+}
+
+const fareDoubler = createFareMultiplier(2)
+
+const fareTripler = createFareMultiplier(3)
+
+function selectDifferentDrivers(drivers,twoDrivers){
+    return twoDrivers(drivers)
+}
+
+// selectDifferentDrivers(drivers, returnLastTwoDrivers)
+// selectDifferentDrivers(drivers, returnFirstTwoDrivers)
